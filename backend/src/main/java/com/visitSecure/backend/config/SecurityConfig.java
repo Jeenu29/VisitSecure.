0c4b28f507f7993eb069a70e1e19a730740f613a
Jeenu Bishnoi <bishnoijeenu2006@gmail.com>
2026-06-23 19:31:20 +0530

    Debugging CORS

--- backend/src/main/java/com/visitSecure/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/visitSecure/backend/config/SecurityConfig.java
@@ -26,9 +26,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .requestMatchers("/api/**").permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 );
 
         return http.build();
